cd6d3052b341eaaa509077fa2adfce5e457c561c
sw4q3ar2 <biggercsgo@gmail.com>
2023-02-24 13:21:34 +0100

    QRkod

---
+++ QRkod.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package metodusok;
+
+/**
+ *
+ * @author sw4q3
+ */
+public class QRkod {
+    
+}
