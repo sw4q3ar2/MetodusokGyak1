d2f21701b171f55f26ddddd8375865b716581cea
sw4q3ar2 <biggercsgo@gmail.com>
2023-02-24 12:43:05 +0100

    booleannal

--- Equalizer.java
+++ Equalizer.java
@@ -13,22 +13,34 @@ public class Equalizer {
     public static void equalizer_5_sor() {
         
         eq();
-        eq(12);
+        eq(15,true);
         eq();
-        eq(2);
+        eq(6, true);
         eq();
         
     }
     
     public static void eq() {
-        int db = rnd.nextInt(3,8);
-        eq(db);
+        eq(false);
+    }
+
+    public static void eq(int db) {
+        eq(db, false);
     }
     
-    public static void eq(int hossz) {
+    public static void eq(boolean ertekLatszik) {
+        int db = rnd.nextInt(7-3+1)+3;
+        eq( db, ertekLatszik);
+    }
+    
+    public static void eq(int db, boolean ertekLatszik) {
         String szin = "\u001B[45m";
-        for (int i = 0; i < hossz; i++) {
+        for (int i = 0; i < db; i++) {
             System.out.print(szin + " ");
+        }
+        if(ertekLatszik){
+            szin = "\u001B[40m";
+            System.out.print(szin + "" + "(" + db + ")");
         }
         System.out.println("");
     }
