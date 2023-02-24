29700630c094c8e90f740e79b4a286819f359aa0
sw4q3ar2 <biggercsgo@gmail.com>
2023-02-24 12:03:01 +0100

    orai

--- Equalizer.java
+++ Equalizer.java
@@ -1,8 +1,35 @@
 package metodusok;
 
+
+import java.util.Random;
+
 public class Equalizer {
     
+    private static Random rnd = new Random();
+    
     public static void main(String[] args) {
+        equalizer_5_sor();
+    }
+    public static void equalizer_5_sor() {
+        
+        eq();
+        eq();
+        eq();
+        eq();
+        eq();
+        
+    }
+    
+    public static void eq() {
+        int db = rnd.nextInt(3,8);
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin + " ");
+        }
+        System.out.println("");
+    }
+    
+    public static void eq(int hossz) {
         
     }
 }
